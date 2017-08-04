c08e56768a416f8aa3e9ac703545a3c87c6fbd0a
jecihjoy <jecihjoy@gmail.com>
2017-08-04 15:15:25 +0300

    003: Code clean up

--- interfaces/postfeedbacktoslack.js
+++ interfaces/postfeedbacktoslack.js
@@ -1,24 +1,28 @@
-var WebClient = require('@slack/client').WebClient;
+const WebClient = require('@slack/client').WebClient;
 const slackconfig = require('../config/slackconfig.json');
 
 
 function sendUserFeedBack(message) {
 
-    var token = slackconfig.slack.bottoken;
-    var channel = slackconfig.slack.channelID;
+    let token = slackconfig.slack.bottoken;
+    let channel = slackconfig.slack.channelID;
 
-    var web = new WebClient(token);
-    web.chat.postMessage(channel, message, function(err, res) {
-        if (err) {
-            console.log('Error:', err);
-        } else {
-            console.log('Message sent: ', res);
-        }
+    return new Promise(function (resolve, reject) {
+        let web = new WebClient(token);
+        web.chat.postMessage(channel, message, function(err, res) {
+            if (err) {
+                console.log('Error:', err);
+                resolve({ status: err });
+            } else {
+                console.log('Message sent: ', res);
+                resolve({ status: 'okay' });
+            }
+        });
+
     });
-
 }
-var postfeedback = {
-    sendUserFeedBack: sendUserFeedBack,
-};
+        var postfeedback = {
+            sendUserFeedBack: sendUserFeedBack,
+        };
 
-module.exports.postfeedback = postfeedback;
+    module.exports.postfeedback = postfeedback;
